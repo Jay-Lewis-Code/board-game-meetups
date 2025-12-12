c090fba9ec6a0d54a99b1db1514bf1fa0f3a1d72
Jason Lewis <lewisjc@my.lanecc.edu>
2025-12-11 16:29:58 -0800

    changes for config staic dist on backend

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/~lewisj628/CS295R/board-game-meetups/', // adjust to the folder that contains index.html
+  //base: '/~lewisj628/CS295R/board-game-meetups/', // adjust to the folder that contains index.html
+  base: '/', // served from http://citweb.lanecc.edu:5028/  
   plugins: [
     react(),
     tailwindcss(),
